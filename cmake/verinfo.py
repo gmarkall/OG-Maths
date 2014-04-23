12047ab1316a6365ea1381a05dcb09a848b0a9c3
Graham Markall <graham@opengamma.com>
2014-04-23 11:25:19 +0100

    [MAT-378] Add subproject to version info

--- cmake/verinfo.py
+++ cmake/verinfo.py
@@ -29,15 +29,24 @@ def platform_code():
     else:
         return 'win'
 
-def generate_verinfo(project, version, revision):
+def get_subprojects(lapack_verinfo_file):
+    with open(lapack_verinfo_file, 'r') as f:
+        lapack_verinfo = load(f, Loader=Loader)
+    if len(lapack_verinfo['platforms']) != 1 or lapack_verinfo['platforms'][0] != platform_code():
+        raise RuntimeError('OG-Lapack platform mismatch')
+    return [{ 'project': lapack_verinfo['project'], 'revision': lapack_verinfo['revision'] }]
+
+def generate_verinfo(project, version, revision, lapack_verinfo_file):
     artifacts = [ jarname (version, jar) for jar in jars ]
+    subprojects = get_subprojects(lapack_verinfo_file)
     d = { 'project': 'OG-Maths', 'revision': revision, 'version': version, \
-          'platforms': [platform_code()], 'artifacts': artifacts }
+          'platforms': [platform_code()], 'artifacts': artifacts,
+          'subprojects': subprojects }
     return d
 
 def main():
-    if len(sys.argv) != 3:
-        print "Usage: verinfo.py <output_file> <version>"
+    if len(sys.argv) != 4:
+        print "Usage: verinfo.py <output_file> <version> <lapack_verinfo>"
         return 1
 
     process = subprocess.Popen(['git', 'rev-parse', 'HEAD'], stdout=subprocess.PIPE)
@@ -47,7 +56,7 @@ def main():
         raise RuntimeError('Error getting SHA1 of git HEAD')
 
     with open(sys.argv[1],'w') as f:
-        f.write(dump(generate_verinfo('OG-Maths', sys.argv[2], rev.strip()), Dumper=Dumper))
+        f.write(dump(generate_verinfo('OG-Maths', sys.argv[2], rev.strip(), sys.argv[3]), Dumper=Dumper))
     return 0
 
 if __name__ == '__main__':
